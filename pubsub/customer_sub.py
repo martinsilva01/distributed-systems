15b29a6846ed6a3e266b4a6218ae1eca5a2803f1
Danthecalfman <spikeeboy@outlook.com>
2025-11-03 23:33:53 -0800

    sub section first attempt

--- pubsub/customer_sub.py
+++ pubsub/customer_sub.py
@@ -2,25 +2,48 @@ import pika
 import time
 import json
 import random
+import threading
+import queue
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+class SubscriberThread(threading.Thread):
+    
+    def __init__(self, message_queue):
+        super().__init__
+        self.message_queue = message_queue
 
-channel = connection.channel()
-channel.exchange_declare(exchange = 'delivery', exchange_type='topic')
-channel.queue_declare(queue='customer_updates')
-channel.queue_bind(exchange = 'delivery', queue = 'customer_updates', routing_key = 'driver.location.1')
 
-print('Waiting for logs.')
-
-def callback(ch, method, properties, body):
+def callback(self, ch, method, properties, body):
     message = body.decode('utf-8')
-    print(f"{message}")
+    self.message_queue.put(message)
 
-channel.basic_consume(queue='customer_updates', on_message_callback=callback, auto_ack=True)
 
-channel.start_consuming()
+def run(self):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
 
-time.sleep(40)
+    channel = connection.channel()
+    channel.exchange_declare(exchange = 'delivery', exchange_type='topic')
+    channel.queue_declare(queue='customer_updates')
+    channel.queue_bind(exchange = 'delivery', queue = 'customer_updates', routing_key = 'driver.location.1')
 
-connection.close()
+    print('Waiting for logs.')
+
+
+    channel.basic_consume(queue='customer_updates', on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
+
+    time.sleep(40)
+
+    connection.close()
+
+def main():
+    message_queue = queue.Queue()
+
+    location_thread = SubscriberThread(message_queue)
+    location_thread.start()
+
+    while True:
+        message = message_queue.get()
+        print(message)
+        time.sleep(2)
