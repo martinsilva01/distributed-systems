80bb5b768860e6a413c247c93c3db6df0c0d2b4f
Danthecalfman <spikeeboy@outlook.com>
2025-11-04 16:19:50 -0800

    fixed some syntax

--- pubsub/customer_sub.py
+++ pubsub/customer_sub.py
@@ -8,34 +8,34 @@ import queue
 class SubscriberThread(threading.Thread):
     
     def __init__(self, message_queue):
-        super().__init__
+        super().__init__()
         self.message_queue = message_queue
 
 
-def callback(self, ch, method, properties, body):
-    message = body.decode('utf-8')
-    self.message_queue.put(message)
+    def callback(self, ch, method, properties, body):
+        message = body.decode('utf-8')
+        self.message_queue.put(message)
 
 
-def run(self):
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost'))
+    def run(self):
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
 
-    channel = connection.channel()
-    channel.exchange_declare(exchange = 'delivery', exchange_type='topic')
-    channel.queue_declare(queue='customer_updates')
-    channel.queue_bind(exchange = 'delivery', queue = 'customer_updates', routing_key = 'driver.location.1')
+        channel = connection.channel()
+        channel.exchange_declare(exchange = 'delivery', exchange_type='topic')
+        channel.queue_declare(queue='customer_updates')
+        channel.queue_bind(exchange = 'delivery', queue = 'customer_updates', routing_key = 'driver.location.1')
 
-    print('Waiting for logs.')
+        print('Waiting for logs.')
 
 
-    channel.basic_consume(queue='customer_updates', on_message_callback=callback, auto_ack=True)
+        channel.basic_consume(queue='customer_updates', on_message_callback=self.callback, auto_ack=True)
 
-    channel.start_consuming()
+        channel.start_consuming()
 
-    time.sleep(40)
+        time.sleep(40)
 
-    connection.close()
+        connection.close()
 
 def main():
     message_queue = queue.Queue()
--- pubsub/driver_pub.py
+++ pubsub/driver_pub.py
@@ -2,23 +2,42 @@ import pika
 import time
 import json
 import random
+import threading
 
+class LocationThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
 
-def get_location():
-    longitude = random.randint(0,1000)
-    lattitute = random.randint(0,1000)
-    return f"Location:{longitude}, {lattitute}"
+    def get_location(self):
+        longitude = random.randint(0,1000)
+        lattitute = random.randint(0,1000)
+        return f"Location:{longitude}, {lattitute}"
+
+    def run(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
+        channel.exchange_declare(exchange = 'delivery', exchange_type='topic')
+        channel.queue_declare(queue='customer_updates')
+        for i in range(5):
+            channel.basic_publish(exchange='delivery', routing_key='driver.location.1', body = self.get_location())
+            print("Sent Location")
+            time.sleep(1)
+        connection.close()
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.exchange_declare(exchange = 'delivery', exchange_type='topic')
-    channel.queue_declare(queue='customer_updates')
+    locationthread = LocationThread()
+    locationthread.start()
+
     for i in range(5):
-        channel.basic_publish(exchange='delivery', routing_key='driver.location.1', body = get_location())
-        print("Sent Location")
-        time.sleep(5)
-    connection.close()
+        print("Im still doing other stuff")
+        time.sleep(1)
+
+    locationthread.join()
+    
+
+
+    
+
 
 
 main()
